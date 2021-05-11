0519a12a1d1753321f5465b66a316dc513207ac1
bendeloyola <bendeloyola2017@gmail.com>
2021-05-11 20:56:02 +0800

    js challenges day 2

--- deloyola-ben/js-challenges-and-projects-v.1.2/intro-to-loops/be-the-bouncer!/app.js
+++ deloyola-ben/js-challenges-and-projects-v.1.2/intro-to-loops/be-the-bouncer!/app.js
@@ -24,3 +24,20 @@ const nightClubRegister = [
         gender: 'male'
     }
 ]
+
+
+for(let i = 0; i < nightClubRegister.length; i++ ){
+    if(nightClubRegister[i].age >= 18){
+        if(nightClubRegister[i].gender === "male"){
+            console.log("Welcome to night club! Mr. " + nightClubRegister[i].name)
+        } else {
+            console.log("Welcome to night club! Ms. " + nightClubRegister[i].name)
+        }
+    } else {
+        if(nightClubRegister[i].gender === "female")
+            console.log("Ms. " + nightClubRegister[i].name + " Sorry you are not allowed here.");
+        else{
+            console.log("Mr. " + nightClubRegister[i].name + " Sorry you are not allowed here.");
+        }
+    }
+}
--- deloyola-ben/js-challenges-and-projects-v.1.2/intro-to-loops/color-my-chessboard!/app.js
+++ deloyola-ben/js-challenges-and-projects-v.1.2/intro-to-loops/color-my-chessboard!/app.js
@@ -1,5 +1,17 @@
 const squares = Array.from(document.querySelectorAll('.grid div'))
 //Nodelist of all the div squares on our board. Think of it as an array. 
 
+
 //Your goal is to add a chessboard color pattern to this blank board using loops and Arrays.
 //write code here
+
+console.log(squares[0]);
+
+for(let i = 0; i < squares.length; i++){
+    squares[i].style.background = "wheat"
+   for(let j = 0; j < squares.length; j++){
+       if(j % 2 === 0){
+        squares[j].style.background = "black"
+       }
+   }
+}
